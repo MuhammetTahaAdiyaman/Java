6322c98152c0c959e1bc5a853f3e0001f477b425
Muhammet Taha Adıyaman <68828015+MuhammetTahaAdiyaman@users.noreply.github.com>
2023-06-04 21:00:51 +0300

    degiskenler.java

---
+++ degiskenler.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    //class isimleri ise PascalCase şeklinde isimlendirilir.
+   //değişkenler camelCase şeklinde isimlendirilir.
+    int not = 55;
+    int kredi = 1000;
+    double notOrtalamasi = 50.5;
+    String isim = "Taha ADIYAMAN";
+    boolean sistemeGirisYapmismi = true;
+    //boolean türü aslında biz genellikle evet veya hayırlı ifadelerde kullanırız. Kontrol amaçlı kullanıyoruz.
+    
+    System.out.println(not);
+  }
+}
